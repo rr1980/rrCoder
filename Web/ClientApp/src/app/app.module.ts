d175ba6b3bb256a9e37dda06de6ad2a4672166f0
rr1980 <rener1980@googlemail.com>
2019-03-23 21:02:25 +0100

    partial

--- Web/ClientApp/src/app/app.module.ts
+++ Web/ClientApp/src/app/app.module.ts
@@ -5,26 +5,33 @@ import { AppRoutingModule, routedComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { httpInterceptorProviders } from './helper/auth-interceptor';
-import { DevExtremeModule } from "devextreme-angular"; 
+import { DevExtremeModule } from "devextreme-angular";
 
+import { PrismModule } from '@sgbj/angular-prism';
+
+import 'prismjs/prism';
+import 'prismjs/components/prism-typescript';
+//import 'prismjs/components/prism-scss';
+//import 'prismjs/components/prism-markup';
 
 
 @NgModule({
-   declarations: [
-      AppComponent,
-      routedComponents
-   ],
-   imports: [
-      BrowserModule,
-      HttpClientModule,
-      DevExtremeModule,
-      AppRoutingModule
-   ],
-   providers: [
-      httpInterceptorProviders
-   ],
-   bootstrap: [
-      AppComponent
-   ]
+  declarations: [
+    AppComponent,
+    routedComponents
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    DevExtremeModule,
+    PrismModule,
+    AppRoutingModule
+  ],
+  providers: [
+    httpInterceptorProviders
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
