bb564c88e91d8da9332ed0ad295fdfeab842e794
rr1980 <rener1980@googlemail.com>
2019-03-25 19:54:57 +0100

    nested/named router outlets

--- Web/ClientApp/src/app/home/home-routing.module.ts
+++ Web/ClientApp/src/app/home/home-routing.module.ts
@@ -5,12 +5,24 @@ import { AuthGuard } from '../helper/auth-guard';
 import { HomeComponent } from './home.component';
 import { NavBarUpComponent } from './nav-bar-up/nav-bar-up.component';
 import { NavBarSideComponent } from './nav-bar-side/nav-bar-side.component';
+import { CodeViewerComponent } from '../code-viewer/code-viewer.component';
 
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent, canActivate: [AuthGuard], data: { roles: ["User"] } },
+  {
+    path: 'home', component: HomeComponent, canActivate: [AuthGuard], data: { roles: ["User"] }, children: [
+
+      { path: 'sideBar1', component: NavBarSideComponent, outlet: 'sideBar' },
+      { path: '', redirectTo: 'sideBar1', pathMatch: 'full', outlet: 'sideBar' },
+      { path: '**', redirectTo: 'sideBar1', outlet: 'sideBar' },
+
+      { path: 'codeContent', component: CodeViewerComponent, outlet: 'content' },
+      { path: '', redirectTo: 'codeContent', pathMatch: 'full', outlet: 'content' },
+      { path: '**', redirectTo: 'codeContent', outlet: 'content' },
+    ]
+  },
   { path: '**', redirectTo: 'home' }
 ];
 
